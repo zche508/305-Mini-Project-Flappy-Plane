668269fbfcf9867d29333c118eed3da3496d8079
Akansha Aggarwal <100277469+akansha944@users.noreply.github.com>
2024-05-25 15:32:13 +1200

    menu

--- 305_files/MiniprojectResources/ball.vhd
+++ 305_files/MiniprojectResources/ball.vhd
@@ -7,26 +7,26 @@ USE  IEEE.STD_LOGIC_UNSIGNED.all;
 ENTITY ball IS
 	PORT
 		( clk 						: IN std_logic;
-		  pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
+		  cursor_row, cursor_column	: IN std_logic_vector(9 DOWNTO 0);
 		  red, green, blue 			: OUT std_logic);		
 END ball;
 
 architecture behavior of ball is
 
-SIGNAL ball_on					: std_logic;
+SIGNAL cursor_on					: std_logic;
 SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
-SIGNAL ball_y_pos, ball_x_pos	: std_logic_vector(9 DOWNTO 0);
+SIGNAL cursor_y_pos, cursor_x_pos	: std_logic_vector(9 DOWNTO 0);
 
 BEGIN           
 
-size <= CONV_STD_LOGIC_VECTOR(8,10);
--- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);
-ball_y_pos <= CONV_STD_LOGIC_VECTOR(350,10);
+size <= CONV_STD_LOGIC_VECTOR(4,10);
+-- cursor_x_pos and cursor_y_pos show the (x,y) for the centre of ball
+cursor_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);
+cursor_y_pos <= CONV_STD_LOGIC_VECTOR(350,10);
 
 
-ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + size) and ('0' & pixel_column <= ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+cursor_on <= '1' when ( ('0' & cursor_x_pos <= cursor_column + size) and ('0' & cursor_column <= cursor_x_pos + size) 	-- x_pos - size <= cursor_column <= x_pos + size
+					and ('0' & cursor_y_pos <= cursor_row + size) and ('0' & cursor_row <= cursor_y_pos + size) )  else	-- y_pos - size <= cursor_row <= y_pos + size
 			'0';
 
 
@@ -34,8 +34,8 @@ ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + size) and ('0' & pixel
 -- Keeping background white and square in red
 Red <=  '1';
 -- Turn off Green and Blue when displaying square
-Green <= not ball_on;
-Blue <=  not ball_on;
+Green <= not cursor_on;
+Blue <=  not cursor_on;
 
 END behavior;
 
--- 305_files/MiniprojectResources/home_screen.vhd
+++ 305_files/MiniprojectResources/home_screen.vhd
@@ -5,7 +5,7 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY home_screen IS
     PORT (
-        clk, showText : IN std_logic;
+        showText : IN std_logic;
         pixel_row, pixel_column : IN std_logic_vector(9 DOWNTO 0);
         pb1 : IN std_logic;  -- Button input for selecting modes
         red, green, blue : OUT std_logic;
@@ -16,58 +16,81 @@ END ENTITY home_screen;
 ARCHITECTURE behavior OF home_screen IS
     TYPE game_state_type IS (home_screen, training_mode, single_player_mode);
     SIGNAL game_state : game_state_type := home_screen;
-BEGIN
-    PROCESS (clk)
-    BEGIN
-        IF rising_edge(clk) THEN
-            CASE game_state IS
-                WHEN home_screen =>
-                    -- Display the name of the game
-                    IF (pixel_row >= 50 AND pixel_row <= 100) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
-                        red <= '1';
-                        green <= '0';
-                        blue <= '0';
-                    ELSE
-                        red <= '0';
-                        green <= '0';
-                        blue <= '0';
-                    END IF;
+	 
+	 SIGNAL ball_on						: std_logic;
+	 SIGNAL size 							: std_logic_vector(9 DOWNTO 0);  
+	 SIGNAL ball_y_pos					: std_logic_vector(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(240, 10) - size;
+	 SiGNAL ball_x_pos					: std_logic_vector(10 DOWNTO 0);
+	 SIGNAL ball_y_motion				: std_logic_vector(9 DOWNTO 0);
+begin 
+		
+size <= CONV_STD_LOGIC_VECTOR(6,10);
+-- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
+ball_x_pos <= CONV_STD_LOGIC_VECTOR(150, 11);
 
-                    -- Display "Training" button
-                    IF (pixel_row >= 150 AND pixel_row <= 200) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
-                        red <= '0';
-                        green <= '1';
-                        blue <= '0';
-                    END IF;
+ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+			'0';
+			
+-- Colours for the screen, ball, and everything else
+Red <=	'1' when ball_on = '1' else
+			'0';
+			
+Green <= '1' when ball_on = '1' else
+			'1'; 
+			
+Blue <=  '0' when ball_on = '1' else
+			'0';
 
-                    -- Display "Single Player" button
-                    IF (pixel_row >= 250 AND pixel_row <= 300) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
-                        red <= '0';
-                        green <= '0';
-                        blue <= '1';
-                    END IF;
+	 
 
-                    -- Handle button inputs for mode selection
-                    IF (pb1 = '0') THEN
-                        -- If the "Training" button is pressed
-                        IF (pixel_row >= 150 AND pixel_row <= 200) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
-                            game_state <= training_mode;
-                            mode <= "01"; -- Training mode
-                        END IF;
-
-                        -- If the "Single Player" button is pressed
-                        IF (pixel_row >= 250 AND pixel_row <= 300) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
-                            game_state <= single_player_mode;
-                            mode <= "10"; -- Single Player mode
-                        END IF;
-                    END IF;
-                
-                WHEN training_mode =>
-                    mode <= "01"; -- Training mode
-                
-                WHEN single_player_mode =>
-                    mode <= "10"; -- Single Player mode
-            END CASE;
-        END IF;
-    END PROCESS;
+--		CASE game_state IS
+--			 WHEN home_screen =>
+--				  -- Display the name of the game
+--				  IF (pixel_row >= 50 AND pixel_row <= 100) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
+--						red <= '1';
+--						green <= '0';
+--						blue <= '0';
+--				  ELSE
+--						red <= '0';
+--						green <= '0';
+--						blue <= '0';
+--				  END IF;
+--
+--				  -- Display "Training" button
+--				  IF (pixel_row >= 150 AND pixel_row <= 200) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
+--						red <= '0';
+--						green <= '1';
+--						blue <= '0';
+--				  END IF;
+--
+--				  -- Display "Single Player" button
+--				  IF (pixel_row >= 250 AND pixel_row <= 300) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
+--						red <= '0';
+--						green <= '0';
+--						blue <= '1';
+--				  END IF;
+--
+--				  -- Handle button inputs for mode selection
+--				  IF (pb1 = '0') THEN
+--						-- If the "Training" button is pressed
+--						IF (pixel_row >= 150 AND pixel_row <= 200) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
+--							 game_state <= training_mode;
+--							 mode <= "01"; -- Training mode
+--						END IF;
+--
+--						-- If the "Single Player" button is pressed
+--						IF (pixel_row >= 250 AND pixel_row <= 300) AND (pixel_column >= 100 AND pixel_column <= 300) THEN
+--							 game_state <= single_player_mode;
+--							 mode <= "10"; -- Single Player mode
+--						END IF;
+--				  END IF;
+--			 
+--			 WHEN training_mode =>
+--				  mode <= "01"; -- Training mode
+--			 
+--			 WHEN single_player_mode =>
+--				  mode <= "10"; -- Single Player mode
+--		END CASE;
+--		END PROCESS;
 END ARCHITECTURE behavior;
